a20e21b66a4e6f44ef857965ab5f68d337142ed5
petrohonchuk <gonchykp@i.ua>
2022-02-17 21:58:21 +0200

    add file

--- galery.js
+++ galery.js
@@ -442,9 +442,29 @@
 // console.log(num);
 // ===========================================================================================
      
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+//   ];
+//     //  const keys = Object.keys(apartment);
+//     //  const values = Object.values(salaries);
+//     const hexColors = [];
+//   const rgbColors = [];
+//      for(let color of colors){
+//         hexColors.push(color.hex)
+//         rgbColors.push(color.rgb)
+//      }
+//      console.log(hexColors);
+//      console.log(rgbColors);
+
+  // ===============================================================
+
+
+
   
-     
-       
+
      
   
 
